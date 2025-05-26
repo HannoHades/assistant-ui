6fff3d7e0e3b7a990714f087704fb93ebb431ee5
Christian Glassiognon <63924603+heyglassy@users.noreply.github.com>
2025-05-25 23:33:55 -0700

    init

--- examples/with-ai-sdk/app/api/chat/route.ts
+++ examples/with-ai-sdk/app/api/chat/route.ts
@@ -1,10 +1,70 @@
 import { openai } from "@ai-sdk/openai";
 import { frontendTools } from "@assistant-ui/react-ai-sdk";
-import { streamText } from "ai";
+import { jsonSchema, streamText } from "ai";
+import { backendTool } from "assistant-stream/core/tool/tool-types";
+import { backendTools } from "assistant-stream/core/tool/tool-types";
 import { z } from "zod";
 
 export const runtime = "edge";
 export const maxDuration = 30;
+
+// Define the weather tool
+const weatherTool = backendTool({
+  description: "Get weather information",
+  parameters: z.object({
+    location: z.string().describe("Location to get weather for"),
+  }),
+  execute: async ({ location }) => {
+    return {
+      weather: `${location} is sunny`,
+    };
+  },
+});
+
+// TODO: support vercel to our tool definition conversion.
+// const weatherTool2 = tool({
+//   description: "Get weather information",
+//   parameters: z.object({
+//     location: z.string().describe("Location to get weather for"),
+//   }),
+//   execute: async ({ location }) => {
+//     return {
+//       weather: `${location} is sunny`,
+//     };
+//   },
+// });
+
+const dayTool = backendTool({
+  description: "Get the current day of the week",
+  parameters: jsonSchema({
+    type: "object",
+    properties: {
+      timezone: {
+        type: "string",
+        description: "Timezone to get the day for",
+      },
+    },
+    required: ["timezone"],
+    additionalProperties: false,
+  }),
+  execute: async () => {
+    return {
+      day: "Monday",
+    };
+  },
+});
+
+// const rainTool = backendTool({
+//   description: "Get the current rain forecast",
+//   parameters: zv4.object({
+//     expectedPercentage: zv4.number().describe("Expected percentage of rain"),
+//   }),
+//   execute: async ({ expectedPercentage }) => {
+//     return {
+//       rain: `${expectedPercentage}% chance of rain`,
+//     };
+//   },
+// });
 
 export async function POST(req: Request) {
   const { messages, system, tools } = await req.json();
@@ -30,3 +90,12 @@ export async function POST(req: Request) {
 
   return result.toDataStreamResponse();
 }
+
+const BackendTools = backendTools({
+  weather: weatherTool,
+  day: dayTool,
+  // rain: rainTool,
+  // weather2: weatherTool2,
+});
+
+export type BackendTools = typeof BackendTools;
--- examples/with-ai-sdk/app/assistant.tsx
+++ examples/with-ai-sdk/app/assistant.tsx
@@ -3,6 +3,29 @@
 import { AssistantRuntimeProvider } from "@assistant-ui/react";
 import { Thread } from "@/components/assistant-ui/thread";
 import { useChatRuntime } from "@assistant-ui/react-ai-sdk";
+import { frontendTool } from "assistant-stream/core/tool/tool-types";
+import z from "zod";
+import { createToolbox } from "@assistant-ui/react/model-context/tool";
+import { BackendTools } from "./api/chat/route";
+
+const toolbox = createToolbox<BackendTools>()({
+  hi: frontendTool({
+    parameters: z.object({
+      name: z.string(),
+    }),
+    execute: async (args) => {
+      return `Other: ${args.name}`;
+    },
+    render: (args) => <div>Hi: {JSON.stringify(args)} confirmed</div>,
+    // CG TODO: Add disable frontend rendering.
+  }),
+  weather: {
+    render: (args) => <div>Weather: {args.weather}</div>,
+  },
+  day: {
+    render: (args) => <div>Day: {args.day}</div>,
+  },
+});
 
 export const Assistant = () => {
   const runtime = useChatRuntime({
@@ -10,7 +33,7 @@ export const Assistant = () => {
   });
 
   return (
-    <AssistantRuntimeProvider runtime={runtime}>
+    <AssistantRuntimeProvider runtime={runtime} toolbox={toolbox}>
       <div className="grid h-dvh gap-x-2 px-4 py-4">
         <Thread />
       </div>
